6402f0d21548cec33da16beea854d0c379b5a623
kshravi86 <kshravi86@gmail.com>
2018-06-16 19:36:13 +0530

    jhv

--- js/admob.js
+++ js/admob.js
@@ -1,8 +1,8 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-5830283354936773/2492236554',
-       interstitial: 'ca-app-pub-5830283354936773/5394689929'
+        banner: 'ca-app-pub-5830283354936773/8496124279',
+       interstitial: 'ca-app-pub-5830283354936773/8276632424'
 	   
 	   
     };
